63e9939ba5260d858db5bdc3c95782a5114ec35f
ljlcarrien <ljlcarrien@icloud.com>
2021-06-03 00:21:56 +0800

    Update 栈的应用-计算四则表达式 更新文档

--- Program.cs
+++ Program.cs
@@ -10,11 +10,11 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            var p1 = ExpressionsHelper.GetPostfixExpression("9+(3-1 1)*3+10/2");
+            var p1 = ExpressionsHelper.GetPostfixExpression("9+(3-1)*3+10/2");
             var v1 = ExpressionsHelper.GetPostfixExpressionResult(p1);
             Console.WriteLine();
-            //var p2 = ExpressionsHelper.GetPostfixExpression("1+2*3+(4*5+6)*7");
-            //var v2 = ExpressionsHelper.GetPostfixExpressionResult(p2);
+            var p2 = ExpressionsHelper.GetPostfixExpression("1+2*3+(4*5+6)*7");
+            var v2 = ExpressionsHelper.GetPostfixExpressionResult(p2);
         }
     }
 }
